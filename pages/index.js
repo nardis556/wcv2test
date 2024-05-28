0adb8b202afbf6b53ee0cee1fbde674e93669019
nardis556 <nardis55five@gmail.com>
2024-05-28 17:20:17 +0000

    asdasdasdasdas

--- pages/index.js
+++ pages/index.js
@@ -89,10 +89,11 @@ export default function Home() {
         try {
           const provider = await EthereumProvider.init({
             projectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID,
-            chains: [64002],
+            chains: [64002, 1],
             showQrModal: true,
             rpcMap: {
               64002: "https://xchain-testnet-rpc.idex.io/",
+              1: "https://eth.llamarpc.com"
             },
             metadata: {
               name: "wcv2test",
@@ -103,12 +104,14 @@ export default function Home() {
               ],
             },
           });
+          provider.on
           return provider;
         } catch (e) {
           console.log(e);
           return e;
         }
       };
+
 
       const provider = await initEtheruemProvider();
 
@@ -117,6 +120,8 @@ export default function Home() {
       const web3Provider = new ethers.BrowserProvider(provider);
       const signer = web3Provider.getSigner();
       const account = await (await signer).getAddress();
+
+      console.log(account)
 
       console.log("Wallet connected:", account);
 
